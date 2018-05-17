c06196a6ae8b7de13f408b8557c1822a5bba9985
Bas Jansen <b@sjansen.email>
2018-05-18 00:27:15 +0200

    Added styling and logo to the sidebar

--- src/components/sidebar/sidebar.vue
+++ src/components/sidebar/sidebar.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   div.sidebar
-    h1 sidebar
+    img.logo(src="~assets/images/logo.svg")
 </template>
 
 <script>
@@ -11,5 +11,16 @@ export default {
 
 <style lang="stylus">
 @import '~styles/variables.styl'
+
+.logo
+  display: block
+  margin: auto
+  width 75%
+  height auto
+
+.sidebar
+  background: #00A5FF
+  box-shadow: 3px 0 10px 0 rgba(0,0,0,0.50)
+  z-index 2
 
 </style>
