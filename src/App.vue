d802c72ba4d9244404eda064b9c87bb172e1a225
Bas Jansen <b@sjansen.email>
2018-05-22 00:59:50 +0200

    Removed most of the auth from app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template lang="pug">
   div#app
-    sidebar(v-if="!multipleHomeys")
-    router-view(v-if="!multipleHomeys")
-    div(v-if="multipleHomeys")
-      ul
-        li(v-for="homey in allHomeys" v-on:click="selectHomey(homey.id)" :key="homey.id") {{homey.name}}
+    sidebar
+    router-view
 </template>
 
 <script>
 import sidebar from '@/components/sidebar/sidebar'
-import Vue from 'vue'
-import queryString from 'query-string'
-import lodash from 'lodash'
+// import Vue from 'vue'
+// import queryString from 'query-string'
+// import lodash from 'lodash'
 
 export default {
   name: 'App',
@@ -21,38 +18,17 @@ export default {
       allHomeys: []
     }
   },
-  methods: {
-    async selectHomey (id) {
-      this.multipleHomeys = await false
-      let selectedHomey = await this.$homeyAPI.forHomeyObject(lodash.find(this.allHomeys, function (homey) {
-        return homey.id === id
-      }))
-      Vue.prototype.$homey = await selectedHomey
-    }
-  },
+  // methods: {
+  //   async selectHomey (id) {
+  //     this.multipleHomeys = await false
+  //     let selectedHomey = await this.$homeyAPI.forHomeyObject(lodash.find(this.allHomeys, function (homey) {
+  //       return homey.id === id
+  //     }))
+  //     Vue.prototype.$homey = await selectedHomey
+  //   }
+  // },
   async created () {
-    let token = window.localStorage.getItem('token')
-    if (token) {
-      token = await this.$athomCloud.setToken(JSON.parse(token))
-    }
-    let qs = queryString.parse(window.location.search)
-    if (qs.code) {
-      token = await this.$athomCloud.authenticateWithAuthorizationCode(qs.code)
-      window.history.pushState({}, '', '/')
-    }
-    if (!token) {
-      document.location.href = this.$athomCloud.getLoginUrl()
-    }
-    let user = await this.$athomCloud.getAuthenticatedUser()
-    let userHomeys = await user.getHomeys()
-    this.allHomeys = userHomeys
-    if (userHomeys.length === 1) {
-      this.multipleHomeys = false
-      let firstHomey = await this.$homeyAPI.forHomeyObject(userHomeys[0])
-      Vue.prototype.$homey = await firstHomey
-    } else {
-      this.multipleHomeys = true
-    }
+
   },
   components: {
     sidebar
