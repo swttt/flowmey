25f0d3601bd65b3728588117159b63762c496617
Bas Jansen <b@sjansen.email>
2018-05-22 01:00:09 +0200

    Added tree view

---
+++ src/components/sidebar/items.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+div.item
+  ul
+    li.child(v-for="item in obj")
+      span.icon
+        i.fas.fa-random.has-text-white(v-if="item.__athom_api_type === 'HomeyAPI.ManagerFlow.Flow'")
+        i.fas.fa-folder(v-else)
+      span.has-text-white(v-if="item.__athom_api_type === 'HomeyAPI.ManagerFlow.Flow'") {{item.title}}
+      span(v-else) {{item.title}}
+      items(v-if="item.children" v-bind:obj="item.children")
+
+</template>
+
+<script>
+import items from '@/components/sidebar/items'
+
+export default {
+  name: 'items',
+  props: ['obj'],
+  components: {
+    items
+  }
+}
+
+</script>
+
+<style lang="stylus">
+
+ul li
+  list-style none
+  margin-left 15px
+  position relative
+  padding-left 5px
+  font-size 14px
+
+
+</style>
--- src/components/sidebar/sidebar.vue
+++ src/components/sidebar/sidebar.vue
@@ -1,17 +1,69 @@
 <template lang="pug">
   div.sidebar
+
     img.logo(src="~assets/images/logo.svg")
+
     div.field
       p.control.has-icons-left
         input.input(type="search" placeholder="Search...")
         span.icon.is-left
           i.fas.fa-search
 
+    items(v-bind:obj="flowsandfolders")
+
 </template>
 
 <script>
+import lodash from 'lodash'
+import items from '@/components/sidebar/items'
+
 export default {
-  name: 'Sidebar'
+  name: 'Sidebar',
+  components: {
+    items
+  },
+  data () {
+    return {
+      folders: {},
+      flows: {}
+    }
+  },
+  created () {
+    this.$homey.flow.getFolders()
+      .then(result => {
+        this.folders = result
+      })
+
+    this.$homey.flow.getFlows()
+      .then(result => {
+        this.flows = result
+        console.log(this.convert(false))
+      })
+  },
+  methods: {
+    convert (folder) {
+      var arr = Object.assign(this.flows, this.folders)
+      arr = lodash.orderBy(arr, 'order')
+      var out = []
+      for (var i in arr) {
+        var obj = arr[i]
+        if (obj.folder === folder) {
+          var children = this.convert(obj.id)
+
+          if (children.length) {
+            obj.children = children
+          }
+          out.push(obj)
+        }
+      }
+      return out
+    }
+  },
+  computed: {
+    flowsandfolders: function () {
+      return this.convert(false)
+    }
+  }
 }
 </script>
 
