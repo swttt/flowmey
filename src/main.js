418419107d79e5191710a9c5f46d7289892bd3b3
Bas Jansen <b@sjansen.email>
2018-05-22 00:59:33 +0200

    Changed auth Force selected first homey for now.

--- src/main.js
+++ src/main.js
@@ -3,12 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import querystring from 'query-string'
+// import lodash from 'lodash'
 
 require('styles/main.styl')
 require('styles/bulma.sass')
-
-// Bulma imports
-// require('bulma/sass/utilities/_all')
 
 Vue.config.productionTip = false
 
@@ -29,12 +28,57 @@ Vue.prototype.$athomCloud
     window.localStorage.removeItem('token')
   })
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: {
-    App
-  },
-  template: '<App/>'
-})
+// Homey Auth
+let token = window.localStorage.getItem('token')
+let qs = querystring.parse(window.location.search)
+
+if (token) {
+  Vue.prototype.$athomCloud.setToken(JSON.parse(token))
+    .then(result => {
+      token = result
+      init()
+    })
+} else if (!token && qs.code) {
+  Vue.prototype.$athomCloud.authenticateWithAuthorizationCode(qs.code)
+    .then(result => {
+      token = result
+      window.history.pushState({}, '', '/')
+      init()
+    })
+} else {
+  document.location.href = Vue.prototype.$athomCloud.getLoginUrl()
+}
+
+function init () {
+  Vue.prototype.$athomCloud.getAuthenticatedUser()
+    .then(user => {
+      let homeys = user.getHomeys()
+      Vue.prototype.$homeyAPI.forHomeyObject(homeys[0])
+        .then(result => {
+          Vue.prototype.$homey = result
+          loadVue()
+        })
+      // FIX HOMEY SELECTOR!
+      // if (homeys.length === 1) {
+      //   Vue.prototype.$homeyAPI.forHomeyObject(homeys[0])
+      //     .then(result => {
+      //       Vue.prototype.$homey = result
+      //       loadVue()
+      //     })
+      // } else {
+      //   console.log('multiple homeys')
+      // }
+    })
+}
+
+function loadVue () {
+  /* eslint-disable no-new */
+  new Vue({
+    el: '#app',
+    router,
+    components: {
+      App
+    },
+    template: '<App/>'
+  })
+}
