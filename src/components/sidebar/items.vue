4069ed3198b95cca9341623a9ce683074f5284ea
SailingDuck <SailingDuck@users.noreply.github.com>
2018-05-23 00:05:22 +0200

    Fix icons

--- src/components/sidebar/items.vue
+++ src/components/sidebar/items.vue
@@ -2,11 +2,16 @@
 div.item
   ul
     li.child(v-for="item in obj")
-      span.icon
-        i.fas.fa-random.has-text-white(v-if="item.__athom_api_type === 'HomeyAPI.ManagerFlow.Flow'")
-        i.fas.fa-folder(v-else)
-      span.has-text-white(v-if="item.__athom_api_type === 'HomeyAPI.ManagerFlow.Flow'") {{item.title}}
-      span(v-else) {{item.title}}
+      div
+        span.has-text-white(v-if="item.__athom_api_type === 'HomeyAPI.ManagerFlow.Flow'")
+          icon
+            i.fas.fa-random.has-text-white
+          | {{item.title}}
+        span(v-else)
+          icon
+            i.fas.fa-folder
+          | {{item.title}}
+
       items(v-if="item.children" v-bind:obj="item.children")
 
 </template>
@@ -32,5 +37,7 @@ ul li
   position relative
   padding-left 5px
   font-size 14px
+  icon
+    padding 3px
 
 </style>
